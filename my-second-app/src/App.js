942a66cb8b87a27746534f92b161106b650d6ded
jazz287 <78864460+jazz287@users.noreply.github.com>
2023-09-17 23:58:52 +0530

    ..

--- my-second-app/src/App.js
+++ my-second-app/src/App.js
@@ -1,6 +1,14 @@
-
 function App() {
-  return <div> hello !</div>
-
+  return (
+    <div>
+      <h1> My Todos </h1>
+      <div className = 'card' >
+        <h2> TITLE </h2>
+        <div>
+          <button> Delete </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default App;
